4a151f49344abe44f93979554f3dc88479ca5dea
Jimmy Jiang <jzy_super2004@163.com>
2018-11-05 21:46:41 +0800

    seg_conn.sh
    Run seg_conn.sh <content> <db_name> to connect to a specific segment in utility mode.

---
+++ scripts/seg_conn.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#variable initialization
+seg_no=$1
+db_name=$2
+
+([ x"${seg_no}"==x ] || [ x"${db_name}"==x ]) && echo "Usage: $0 [content_number] [dbname]" && exit 1;
+
+#generate tmp file that contains 
+sql_to_execute="
+\o /tmp/seg_conn.out
+select content, hostname, port from gp_segment_configuration where role = 'p';
+"
+nohup psql postgres <<EOF
+    ${sqls_to_execute}
+EOF
+
+#concat CONNECT string
+host_name=`cat /tmp/seg_conn.out | sed 's/[ ]*//g' | grep ^${seg_no}'|' | awk -F'|' '{print $2}'`
+seg_port=`cat /tmp/seg_conn.out | sed 's/[ ]*//g' | grep ^${seg_no}'|' | awk -F'|' '{print $3}'`
+
+#execute
+PGOPTIONS='-c gp_session_role=utility' psql $db_name -h $host_name -p $seg_port
