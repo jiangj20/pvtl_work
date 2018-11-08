653fd99197acc37f8916ff5d5c07793c9e2dc976
Jimmy Jiang <jzy_super2004@163.com>
2018-11-08 09:43:59 +0800

    Update seg_conn.sh

--- scripts/seg_conn.sh
+++ scripts/seg_conn.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-#variable initialization
+
+#variable definition
 seg_no=$1
 db_name=$2
-
-([ x"${seg_no}"==x ] || [ x"${db_name}"==x ]) && echo "Usage: $0 [content_number] [dbname]" && exit 1;
-
-#generate tmp file that contains 
-sql_to_execute="
-\o /tmp/seg_conn.out
+file_name=$3
+sql_check_conf="
+\o /tmp/seg_conn.out;
 select content, hostname, port from gp_segment_configuration where role = 'p';
 "
+
+#check arguments
+[ $# -gt 3 ] && echo "Too many arguments." && exit 1;
+([ x"${seg_no}" == x ] || [ x"${db_name}" == x ]) && echo "Usage: $0 <content_number> <dbname> [file_to_run]" && exit 1;
+
+#generate tmp file that contains seg info
 nohup psql postgres <<EOF
-    ${sqls_to_execute}
+    ${sql_check_conf}
 EOF
 
 #concat CONNECT string
@@ -19,4 +23,8 @@ host_name=`cat /tmp/seg_conn.out | sed 's/[ ]*//g' | grep ^${seg_no}'|' | awk -F
 seg_port=`cat /tmp/seg_conn.out | sed 's/[ ]*//g' | grep ^${seg_no}'|' | awk -F'|' '{print $3}'`
 
 #execute
-PGOPTIONS='-c gp_session_role=utility' psql $db_name -h $host_name -p $seg_port
+if [ x"${file_name}" == x ]; then
+PGOPTIONS='-c gp_session_role=utility' psql ${db_name} -h ${host_name} -p ${seg_port}
+else
+PGOPTIONS='-c gp_session_role=utility' psql ${db_name} -h ${host_name} -p ${seg_port} -f ${file_name}
+fi
